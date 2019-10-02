311ef519d7b63d6fb89f76b04ed404920e926a8a
benmcdermott <benmcdermott31@gmail.com>
2019-10-02 17:15:13 +0100

    Optimized 2D spectrum calculation

--- spectra.py
+++ spectra.py
@@ -3,49 +3,58 @@
 
 import numpy as np
 
-# to sort wavenumbers, and sort specturm in order of increasing wavenumber
-def sort_spec(kmag,E):
+
+# to sort wavenumbers, and sort spectrum in order of increasing wavenumber
+def sort_spec(kmag, E):
     kflat = np.ndarray.flatten(kmag)
-    Eflat = np.ndarray.flatten(np.real(E))
+    Eflat = np.ndarray.flatten(E)
     isort = np.argsort(kflat)
     ksort = kflat[isort]
     Esort = Eflat[isort]
     return ksort, Esort
 
+
 # to bin the spectrum into rings of width dk
-def bin_spec(ksort,Esort,Nspec):
-    k = np.arange(0.0,Nspec,1.0)
-    dk = k[1]-k[0]
-    spec = np.zeros((Nspec,1))
-    for i in range(Nspec):
-        for j in range(len(ksort)):
-            if ksort[j] >= k[i]:
-                if ksort[j] < k[i]+dk:
-                    spec[i] += Esort[j]
+def bin_spec(ksort, Esort, nx):
+    ind = int(nx / 2)
+    k = np.arange(0.0, nx / 2, 1.0)
+    dk = k[1] - k[0]
+    spec = np.zeros((ind, 1))
+    for i in range(ind):
+    # sum energy in each 2D ring at distance |k|, with width dk
+        # np.where finds the indices in ksort where k >= k[i] > k+dk
+        # then we sum the energy in that ring of width dk
+        spec[i] = np.sum(Esort[np.where((ksort >= k[i]) & (ksort < k[i]+dk))])
     return spec
 
+
 # 2D scalar spectrum in a horizontal plane
-def scalar_spec2d(s):
+def scalar_spec2d(s, nx):
+
+    # want a 2D slice
     if len(np.shape(s)) > 2:
         raise ValueError('Input array must be 2D')
+
+    # need number of rows=number of columns
     if np.shape(s)[0] != np.shape(s)[1]:
         raise ValueError('Dimensions of input array must be equal')
 
-    nx = np.shape(s)[0]
-    factor = float(1.0/nx/nx)                                  # fft normalisation factor
-    ind = int(nx/2)
+    factor = 1.0 / nx / nx  # fft normalisation factor
+    ind = int(nx / 2)
     dk = 1.0
-    k = np.arange(0.0,nx/2-1+dk,1.0)                         # positive horizontal wavenumbers
-    s_hat = abs(np.fft.fft2(s))                              # 2D fft
-    s_hat = factor*s_hat[0:ind,0:ind]                   # normalise and cut
+    k = np.arange(0.0, nx / 2 - 1 + dk, 1.0)  # positive horizontal wavenumbers
+    s_hat = abs(np.fft.fft2(s))  # 2D fft
+    s_hat = factor * s_hat[0:ind, 0:ind]  # normalise and cut
 
-    kmag = np.zeros((ind,ind))
-    E = np.zeros((ind,ind))
+    kmag = np.zeros((ind, ind))
+    E = np.zeros((ind, ind))
     for i in range(ind):
         for j in range(ind):
-            kmag[i,j] = np.sqrt(k[i]**2 + k[j]**2)
-            E[i,j] = np.pi*kmag[i,j]*s_hat[i,j]*np.conj(s_hat[i,j])
+            kmag[i, j] = np.sqrt(k[i] ** 2 + k[j] ** 2)
+            E[i, j] = np.pi * kmag[i, j] * s_hat[i, j] * np.conj(s_hat[i, j])
 
-    ksort, Esort = sort_spec(kmag,E)
+    ksort, Esort = sort_spec(kmag, E)
 
-    spec = bin_spec(ksort,Esort,ind)
+    spec = bin_spec(ksort, Esort, nx)
+
+    return k, spec
